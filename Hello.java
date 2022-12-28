980bbe6b200e6bdc47267a924a5a83f94cb869b2
Bibi3100 <bibirabu1997@gmail.com>
2022-12-28 17:29:34 -0500

    Hello.java filr is edited

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        int i =25;
+        float f=12.34f;
+        //System.out.println("rabu");
+       System.out.println("i="+i+" "+"f="+f);
+    }
+}
